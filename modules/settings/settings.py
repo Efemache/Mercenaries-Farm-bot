792c7ffa6a78ad92738bf3e3f2e8f39394d74d6e
Jonathan Reyes <1192712+ChocoShell@users.noreply.github.com>
2022-03-24 01:36:51 -0400

    Add exceptions and move read* functions to file utils

--- modules/settings/settings.py
+++ modules/settings/settings.py
@@ -1,40 +1,39 @@
 import pathlib
 import shutil
-import re
-import json
-import configparser
+
+from modules.exceptions import MissingGameDirectory, UnsetGameDirectory
+from modules.file_utils import parseINI, readINI
 
 
-def readjson(jfile):
-    """... just for reading json file and return data :)"""
-    with open(jfile) as descriptor:
-        data = json.load(descriptor)
-    return data
+def get_settings(settings_filename):
+    """Read settings.ini and put it in a table :
+    Settings -
+        0: MonitorResolution (1920x1080),
+        1: level (20),
+        2: location (The Barrens),
+        3: mode (Heroic),
+        4: quitBeforeBossFight (True),
+        5: heroSet (True),
+        6: monitor (1),
+        7: MouseSpeed (0.5),
+        8: WaitForEXP (3),
+        9: Zonelog (GameDir/Logs/Zone.log)
+    """
+    settings_dict = parseINI(readINI(settings_filename)["BotSettings"])
 
+    if not settings_dict["gamedir"]:
+        raise UnsetGameDirectory("Game Dir setting is not set")
 
-def readINI(inifile, section):
-    """... just for reading .ini file and return data"""
-    config = configparser.ConfigParser()
-    config.read(inifile)
+    game_dir = pathlib.Path(settings_dict["gamedir"])
 
-    return config[section]
+    if not game_dir.is_dir():
+        raise MissingGameDirectory(f"Game directory ({game_dir}) does not exist")
+    else:
+        settings_dict["zonelog"] = pathlib.PurePath(
+            game_dir, "Logs/Zone.log"
+        ).as_posix()
 
-
-def parseINI(inidict):
-    """... just for transform value into right type"""
-    initype = {}
-    for k in inidict.keys():
-        i = inidict[k].split("#")[0]
-        if i in ["True", "False"]:
-            initype[k] = bool(i)
-        elif re.match("^[0-9]+$", i):
-            initype[k] = int(i)
-        elif re.match("^[0-9]+\.[0-9]+$", i):
-            initype[k] = float(i)
-        else:
-            initype[k] = str(i)
-
-    return initype
+    return settings_dict
 
 
 def copy_config_from_sample_if_not_exists(filename):
