3a128c5542dcca8b8c46e2861839c706b7e0168a
Jonathan Reyes <1192712+ChocoShell@users.noreply.github.com>
2022-03-30 17:01:07 -0400

    Removing extra log details

--- modules/__init__.py
+++ modules/__init__.py
@@ -22,7 +22,7 @@ log_config = {
     },
     "formatters": {
         "std_out": {
-            "format": "%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(lineno)d : (Process Details : (%(process)d, %(processName)s), Thread Details : (%(thread)d, %(threadName)s))\nLog : %(message)s",
+            "format": "%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(message)s",
             "datefmt": "%d-%m-%Y %I:%M:%S",
         },
         "old_out": {"format": "%(message)s"},
