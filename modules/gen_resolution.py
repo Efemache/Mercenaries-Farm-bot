6e3e5c0d73279e4270ae51ef659e4b5a9f8672e6
Efemache <56414438+Efemache@users.noreply.github.com>
2022-09-01 18:48:03 +0200

    chg: usr: it's now possible to use natively other resolution

--- modules/gen_resolution.py
+++ modules/gen_resolution.py
@@ -1,45 +1,51 @@
 import cv2
 
-from modules.settings import settings_dict
-from modules.file_utils import copy_dir_and_func_files
+from .settings import settings_dict
+from .file_utils import copy_dir_and_func_files
 
-BASEDIR = "files"
-orig_resolution = "1920x1080"
+import logging
+
+log = logging.getLogger(__name__)
+
+BASEDIR = settings_dict["root_images_dir"]
+orig_resolution = settings_dict["default_resolution"]
 
 
 def resize_image(srcfile, dstfile, params=[]):
-    """resize image from source to destination
-    """
-    resolo = params[0]
-    resold = params[1]
+    """resize image from source to destination"""
+    orig_resolution_w = int(params[0].split("x")[0])
+    new_resolution_w = int(params[1].split("x")[0])
 
     img = cv2.imread(srcfile, cv2.IMREAD_UNCHANGED)
-    scale_percent = int(resold.split("x")[0]) / int(resolo.split("x")[0])
-    width = int(img.shape[1] * scale_percent)
-    height = int(img.shape[0] * scale_percent)
+    scale_ratio = new_resolution_w / orig_resolution_w
+    width = int(img.shape[1] * scale_ratio)
+    height = int(img.shape[0] * scale_ratio)
 
-    # print(f"resize: {imgfile} -> {dst}/{name}")
+    # print(f"resize: {srcfile} -> {dstfile}")
 
     # INTER_AREA INTER_CUBIC INTER_LANCZOS4 INTER_LINEAR INTER_NEAREST
     imgresized = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
     cv2.imwrite(dstfile, imgresized)
 
 
-new_resolution = settings_dict["resolution"]
+def gen_images_new_resolution():
+    new_resolution = settings_dict["resolution"]
 
-ox, oy = orig_resolution.split("x")
-nx, ny = new_resolution.split("x")
+    ox, oy = orig_resolution.split("x")
+    nx, ny = new_resolution.split("x")
 
-if round(int(ox) / int(oy), 2) == round(int(nx) / int(ny), 2) and orig_resolution != new_resolution:
-    copy_dir_and_func_files(
-        BASEDIR,
-        orig_resolution,
-        new_resolution,
-        ".png",
-        resize_image,
-        [orig_resolution, new_resolution],
-    )
-else:
-    print(
-        "Resolution not changed (1920x1080) or doesn't have the same ratio as 1920x1080"
-    )
+    if orig_resolution == new_resolution:
+        log.debug(f"Resolution not changed : {orig_resolution}")
+    else:
+        # Resolution modified so we need to generate images
+        # we check it's the same ratio as the original images
+        if round(int(ox) / int(oy), 2) == round(int(nx) / int(ny), 2):
+            copy_dir_and_func_files(
+                f"{BASEDIR}/{orig_resolution}",
+                f"{BASEDIR}/{new_resolution}",
+                ".png",
+                resize_image,
+                [orig_resolution, new_resolution],
+            )
+        else:
+            log.error(f"Resolution doesn't have the same ratio as {orig_resolution}")
