f759c07cadc3e432a3ac4005878079339252bae3
Efemache <56414438+Efemache@users.noreply.github.com>
2022-11-02 15:48:34 +0100

    chg: usr: manage new Campfire screen (from 24.6 HS patch)

--- modules/campfire.py
+++ modules/campfire.py
@@ -23,6 +23,65 @@ log = logging.getLogger(__name__)
 # To do : add mouse position in positions.json
 
 
+def toggle_campfire_screen():
+    if find_ellement(Button.campfire_hiddenparty.filename, Action.move_and_click):
+        return "party"
+        time.sleep(2)
+    elif find_ellement(Button.campfire_hiddenvisitors.filename, Action.move_and_click):
+        return "visitor"
+        time.sleep(2)
+    else:
+        return None
+
+
+def check_party_tasks():
+    if find_ellement(Button.campfire_hiddenvisitors.filename, Action.screenshot):
+        waitForItOrPass(Button.campfire_completed_partytask, 3)
+        if find_ellement(
+            Button.campfire_completed_partytask.filename, Action.move_and_click
+        ):
+            while not find_ellement(Button.campfire_claim.filename, Action.screenshot):
+                time.sleep(0.5)
+            return True
+    return False
+
+
+def check_visitor_tasks():
+    if find_ellement(Button.campfire_hiddenparty.filename, Action.screenshot):
+        waitForItOrPass(Button.campfire_completed_task, 3)
+        if (
+            find_ellement(
+                Button.campfire_completed_task.filename, Action.move_and_click
+            )
+            or find_ellement(
+                Button.campfire_completed_eventtask.filename, Action.move_and_click
+            )
+            or find_ellement(
+                Button.campfire_completed_expansiontask.filename,
+                Action.move_and_click,
+            )
+        ):
+            return True
+    return False
+
+
+def claim_task_reward():
+    while not find_ellement(Button.campfire_claim.filename, Action.screenshot):
+        time.sleep(0.5)
+
+    # Loop added beause sometimes the bot find the button but
+    # Hearthstone is not ready, so the bot click too soon.
+    # Need to make a loop to try several time to click
+    while find_ellement(Button.campfire_claim.filename, Action.move_and_click):
+        time.sleep(0.5)
+        move_mouse(windowMP(), windowMP()[2] / 2, windowMP()[3] / 1.125)
+
+    time.sleep(2)
+    while not find_ellement(UIElement.campfire.filename, Action.screenshot):
+        mouse_click()
+        time.sleep(2)
+
+
 def look_at_campfire_completed_tasks():
     """Once opened, look at campfire if you find completed tasks and,
     if so, open them.
@@ -31,41 +90,24 @@ def look_at_campfire_completed_tasks():
     retour = False
     if find_ellement(UIElement.campfire.filename, Action.screenshot):
         retour = True
+        toggled = False
         while find_ellement(UIElement.campfire.filename, Action.move):
-            waitForItOrPass(Button.campfire_completed_task, 3)
             if (
+                find_ellement(Button.campfire_hiddenparty.filename, Action.screenshot)
+                and check_visitor_tasks()
+            ) or (
                 find_ellement(
-                    Button.campfire_completed_task.filename, Action.move_and_click
+                    Button.campfire_hiddenvisitors.filename, Action.screenshot
                 )
-                or find_ellement(
-                    Button.campfire_completed_eventtask.filename, Action.move_and_click
-                )
-                or find_ellement(
-                    Button.campfire_completed_expansiontask.filename,
-                    Action.move_and_click,
-                )
+                and check_party_tasks()
             ):
-                # time.sleep(2)
-                while not find_ellement(
-                    Button.campfire_claim.filename, Action.screenshot
-                ):
-                    time.sleep(0.5)
-
-                # Loop added beause sometimes the bot find the button but
-                # Hearthstone is not ready, so the bot click too soon.
-                # Need to make a loop to try several time to click
-                while find_ellement(
-                    Button.campfire_claim.filename, Action.move_and_click
-                ):
-                    time.sleep(0.5)
-                    move_mouse(windowMP(), windowMP()[2] / 2, windowMP()[3] / 1.125)
-
-                time.sleep(2)
-                while not find_ellement(UIElement.campfire.filename, Action.screenshot):
-                    mouse_click()
-                    time.sleep(2)
+                claim_task_reward()
             else:
-                break
+                if toggled:
+                    break
+                else:
+                    toggle_campfire_screen()
+                    toggled = True
 
         move_mouse_and_click(windowMP(), windowMP()[2] / 1.16, windowMP()[3] / 1.93)
 
