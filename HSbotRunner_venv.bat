e286a2838d7f780bbe0f950ede9561df1b1f5f11
Efemache <56414438+Efemache@users.noreply.github.com>
2023-03-04 23:23:04 +0100

    chg: dev: venv launchers files overwrite standard launchers files

--- HSbotRunner_venv.bat
+++
@@ -1,4 +0,0 @@
-py -3.11 -m venv %~dp0MFB
-%~dp0MFB\Scripts\python.exe -m pip install -r requirements_win.txt
-%~dp0MFB\Scripts\python.exe main.py
-pause
--- HSbotRunner_venv.sh
+++
@@ -1,3 +0,0 @@
-python3.11 -m venv MFB
-./MFB/bin/python -m pip install -r requirements_linux.txt
-./MFB/bin/python main.py
