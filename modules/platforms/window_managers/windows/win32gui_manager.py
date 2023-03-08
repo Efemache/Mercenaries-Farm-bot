8c630dad107754b5ebf5f53d05dc4201f888111c
pujunzhu <pujun.zhu@gmail.com>
2023-03-08 19:16:24 +0100

    1. Fix window not poping up all the time issue. 2. Move reconnect to defaultcase, add clicking when there is nothing found

--- modules/platforms/window_managers/windows/win32gui_manager.py
+++ modules/platforms/window_managers/windows/win32gui_manager.py
@@ -1,7 +1,6 @@
 import re
 import logging
 from ..base import WindowMgr
-from ....settings import settings_dict
 from ...platforms import find_os
 
 log = logging.getLogger(__name__)
@@ -9,6 +8,7 @@ log = logging.getLogger(__name__)
 try:
     import win32gui
     import win32com.client as win32
+
     HAS_WIN32GUI = True
 except ImportError:
     HAS_WIN32GUI = False
@@ -16,10 +16,6 @@ except ImportError:
         log.debug("win32gui not installed")
 
 SW_SHOW = 5
-left = 0
-top = 0
-width = 1920
-height = 1080
 
 
 class WindowMgrWindowsWin32Gui(WindowMgr):
@@ -29,46 +25,39 @@ class WindowMgrWindowsWin32Gui(WindowMgr):
         """Constructor"""
         self._handle = None
 
-    def find_game(self, WINDOW_NAME_WINDOWS):
+    def find_game(self, WINDOW_NAME):
         """find the hearthstone game window"""
-        self._find_window(WINDOW_NAME_WINDOWS)
-        # print(self._handle)
-        if self._handle is not None:
+        self._find_window(WINDOW_NAME)
+        if (self._handle is not None) \
+            &(WINDOW_NAME!=win32gui.GetWindowText(win32gui.GetForegroundWindow())):
             self._show_window()
             self._set_foreground()
         return self._handle
 
     def get_window_geometry(self):
         global left, top, width, height
-        # To get the acitve window name
+        # To get the active window name
         WINDOW_NAME = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
         # Judge which window, fake the BN resolution
-        if WINDOW_NAME == "Hearthstone":
+        if WINDOW_NAME == "Battle.net":
+            return (0, 0, 1920, 1080)
+        else:
             left, top, width, height = win32gui.GetClientRect(self._handle)
             left, top = win32gui.ClientToScreen(self._handle, (left, top))
-        elif WINDOW_NAME == "Battle.net":
-            current_resolution = settings_dict["resolution"]
-            ox, oy = current_resolution.split("x")
-            width = int(ox)
-            height = int(oy)
-            left = 0
-            top = 0
-        else:
-            log.info(WINDOW_NAME)
-        return (left, top, width, height)
+            return (left, top, width, height)
 
-    def _window_enum_callback(self, hwnd, WINDOW_NAME_WINDOWS):
+    def _window_enum_callback(self, hwnd, WINDOW_NAME):
         """Pass to win32gui.EnumWindows() to check all the opened windows"""
-        if re.match(WINDOW_NAME_WINDOWS, str(win32gui.GetWindowText(hwnd))) is not None:
+        if re.match(WINDOW_NAME, str(win32gui.GetWindowText(hwnd))) is not None:
             # print(hwnd)
             self._handles.append(hwnd)
             self._handle = hwnd
 
-    def _find_window(self, WINDOW_NAME_WINDOWS):
+    def _find_window(self, WINDOW_NAME):
         self._handle = None
         self._handles = []
-        win32gui.EnumWindows(self._window_enum_callback, WINDOW_NAME_WINDOWS)
+        win32gui.EnumWindows(self._window_enum_callback, WINDOW_NAME)
 
         if len(self._handles) < 1:
             log.info("Matched no window")
@@ -82,6 +71,8 @@ class WindowMgrWindowsWin32Gui(WindowMgr):
             self._handle = self._handles[0]
 
     def _show_window(self):
+        shell = win32.Dispatch("WScript.Shell")
+        shell.SendKeys("%")
         win32gui.ShowWindow(self._handle, SW_SHOW)
 
     def _set_foreground(self):
