d8fec89501a6970ad01ab580a64a45887a3e5c25
pujunzhu <pujun.zhu@gmail.com>
2023-03-12 11:14:53 +0100

    stability fix

--- main.py
+++ main.py
@@ -15,14 +15,22 @@ log = logging.getLogger(__name__)
 def main():
     log.info(f"Python version: {sys.version}")
     gen_images_new_resolution()
+    #Sometimes it is the first BN window shall be launched, sometimes it is the second.
+    BNCount=1
 
     while True:
         log.info("Loop")
         try:
             if win.find_game("Hearthstone"):
                 where()
-            elif win.find_game("Battle.net"):
+                BNCount=1
+            elif win.find_game("Battle.net", BNCount):
                 enter_from_battlenet()
+                if BNCount==0:
+                    BNCount=1
+                else: 
+                    BNCount=0
+                time.sleep(1)
         except KeyboardInterrupt as kerr:
             log.info("Keyboard Interrupt %s", kerr)
             sys.exit(0)
