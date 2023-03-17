1b893d603e50af50cf1ebaf05812a5f33d10572c
pujunzhu <49236153+pujunzhu@users.noreply.github.com>
2023-03-17 20:41:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+#! /usr/bin/env python3
+import time
+import sys
+
+from modules.gameloop import where
+from modules.battlenetloop import enter_from_battlenet
+from modules.platforms import win
+from modules.resolution import gen_images_new_resolution
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
 def main():
     log.info(f"Python version: {sys.version}")
     gen_images_new_resolution()
-
-    while True:
-        log.info("Loop")
-        try:
-            if win.find_game("Hearthstone"):
-                where()
-            elif win.find_game("Battle.net"):
-                enter_from_battlenet()
+	@@ -26,10 +12,3 @@ def main():
         except KeyboardInterrupt as kerr:
             log.info("Keyboard Interrupt %s", kerr)
             sys.exit(0)
+        except Exception as error:
+            log.error("Error: %s", error)
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
