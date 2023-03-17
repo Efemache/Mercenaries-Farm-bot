e7a868d6ac6b2d2f11978ec59a4d291d1de632ba
pujunzhu <49236153+pujunzhu@users.noreply.github.com>
2023-03-17 20:46:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import sys
 
 from modules.gameloop import where
+from modules.battlenetloop import enter_from_battlenet
 from modules.platforms import win
 from modules.resolution import gen_images_new_resolution
 
@@ -14,11 +15,22 @@ log = logging.getLogger(__name__)
 def main():
     log.info(f"Python version: {sys.version}")
     gen_images_new_resolution()
+    #Sometimes it is the first BN window shall be launched, sometimes it is the second.
+    BNCount=1
+
     while True:
         log.info("Loop")
         try:
-            if win.find_game():
+            if win.find_game("Hearthstone"):
                 where()
+                BNCount=1
+            elif win.find_game("Battle.net", BNCount):
+                enter_from_battlenet()
+                if BNCount==0:
+                    BNCount=1
+                else: 
+                    BNCount=0
+                time.sleep(1)
         except KeyboardInterrupt as kerr:
             log.info("Keyboard Interrupt %s", kerr)
             sys.exit(0)
@@ -29,3 +41,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
